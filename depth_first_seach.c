aed36f0a8ec801b9352e578b019838a3327cc61f
Diljit PR <diljitpr@gmail.com>
2013-11-28 23:57:05 +0530

    Update depth_first_seach.c

--- depth_first_seach.c
+++ depth_first_seach.c
@@ -1,14 +1,45 @@
+
+/* author :diljit ramachandran 
+   date: 23 november 2013
+   language: c
+   licence:gpl
+   free to share,edit,copy and redistribute 
+
+
+
+Note :Test the program with a graph(unweighted/directed or undirected represented by its adjancency matrix) 
+
+Sample Graph can be used from this link : http://www.codeproject.com/KB/java/BFSDFS/graph.PNG  
+
+Functions and Variables Used :
+
+
+void depth_first_search() : takes source as parameter /on the vertex u want to perform dfs
+graph[][] :the adjancey matrix
+visited[] : auxillary matix to keep track of the visited nodes in the traversal
+size : the number of vertices
+source : the source/initial vertex
+
+ */
+
+
+
+
 #include<stdio.h>
 #define size 6
 #define true 1
 #define false 0
-void depth_first_search(int);
+
+void depth_first_search (int);
 int visited[size];
 int graph[size][size];
+
 	int main()
 		{
 		int i,j,source;
 		printf("Enter the adjanceny matrix \n");
+	
+	
 		for(i = 0 ; i < size ; i++)
 		 {
 		        printf("node %d \n",i+1);
@@ -35,11 +66,16 @@ int graph[size][size];
 		 
 		 }
 		 
+		 
+		 
+		 
 	void depth_first_search(int source)
 		{
+			
+		
 		visited[source] = true; // initialize the source as visited i.e true
 		int j;
-			for( j = 0 ; j < size ; j++)			
+			for( j = 0 ; j < size; j++)			
 		   		{
 		   			   
 				if (graph[source][j] == 1 && visited[j] == false) // if the adjacent node is connected and not visited... then call dfs on that adjacent vertex recursively pretty simple :)
@@ -47,7 +83,7 @@ int graph[size][size];
 				
 					 	printf(" --> %d",j);
 							 		 
-						depth_first_search(j); // callinf dfs recursively on the unvisited node
+						depth_first_search(j); // calling dfs recursively on the unvisited node
 			 
 			         	 }
 		   		}
@@ -55,3 +91,12 @@ int graph[size][size];
 		  
 		}
 		   
+		   		   	
+		
+		
+		
+			
+			
+		
+		
+		
